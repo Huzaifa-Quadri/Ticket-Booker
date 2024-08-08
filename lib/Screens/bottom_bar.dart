e58240c4fb345c482a899125f622108a6e7762eb
Huzaifa_q <huzaifaquadri1853@gmail.com>
2024-08-08 13:00:26 +0530

    Initial Commit

---
+++ lib/Screens/bottom_bar.dart
@@ -0,0 +1,53 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_booking/Screens/home.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomNavBar> {
+  int activeIndex = 0;
+  void _OnTappedNav(int index){
+    setState(() {
+      activeIndex = index;
+    });
+
+  }
+
+  static final List<Widget> _widgetoption = <Widget>[
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        // child: Text("Just do as i say !");
+          child: _widgetoption[activeIndex]
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _OnTappedNav,
+        showSelectedLabels: false,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        currentIndex: activeIndex,
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: false,
+        items: const[
+          BottomNavigationBarItem(icon: Icon(FluentIcons.home_12_filled), label: "Home" ),
+          BottomNavigationBarItem(icon: Icon(FluentIcons.search_12_regular), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(FluentIcons.ticket_horizontal_20_filled), label: "Label"),
+          BottomNavigationBarItem(icon: Icon(FluentIcons.person_32_filled), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
